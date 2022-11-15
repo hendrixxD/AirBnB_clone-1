116a9a3eb660f935212769b3314df2cbc00a23f8
hendrixxD <maticsjnr@gmail.com>
2022-11-15 02:55:46 +0100

    /python/<default_text>

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""AirBnB flask app Module"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def return_message():
+    """return a simple message"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def return_HBNB():
+    """return HBNB in /hbnb route"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def C_is_fun(text):
+    """C is fun"""
+    return f"C {text}".replace("_", " ")
+
+
+@app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def default_message(text="is cool"):
+    """overwrites the default text"""
+    return f"Python {text}".replace("_", " ")
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
