f0714af566737c13ef88c0ef0a04225f3d32790c
hendrixxD <maticsjnr@gmail.com>
2022-11-15 02:06:57 +0100

    pycode wahala

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -5,15 +5,18 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def return_message():
     """return a simple message"""
     return "Hello HBNB!"
 
+
 @app.route('/hbnb', strict_slashes=False)
 def return_HBNB():
     """return HBNB in /hbnb route"""
     return "HBNB"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
