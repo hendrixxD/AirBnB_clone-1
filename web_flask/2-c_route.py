271ab11f4d199f93c347b366f482afeb553498d8
hendrixxD <maticsjnr@gmail.com>
2022-11-15 02:29:50 +0100

    /c/<text>

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""AirBnB flask app Module"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def return_message():
+    """return a simple message"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def return_HBNB():
+    """return HBNB in /hbnb route"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def C_is_fun(text):
+    """C is fun"""
+    return f"C {text}".replace("_", " ")
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
