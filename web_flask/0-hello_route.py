29cc762f07580caed06d460a53ccd35490b7f899
hendrixxD <maticsjnr@gmail.com>
2022-11-15 01:48:46 +0100

    a simple retunr message

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""AirBnB flask app Module"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def return_message():
+    """return a simple message"""
+    return "Hello HBNB!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
