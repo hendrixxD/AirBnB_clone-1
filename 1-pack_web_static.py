f7036c750a6da6f45494f256f364a48d91e2f694
hendrixxD <maticsjnr@gmail.com>
2022-12-10 14:06:26 +0100

    fic conflicts

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -2,7 +2,7 @@
 """
  a Fabric script that generates a .tgz archive
  from the contents of the web_static folder of 
- your AirBnB Clone repo, using the function do_pack
+ your AirBnB Clone repo
 """
 
 from fabric.api import local
